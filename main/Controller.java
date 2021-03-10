7b156a012346c921b0ac3673f3bd7f832e386da2
ConnorCurry <connor4424@gmail.com>
2021-03-10 02:21:27 -0500

    Controller Method Headers

--- main/Controller.java
+++ main/Controller.java
@@ -2,15 +2,56 @@ package main;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Date;
 
 public class Controller {
     
     private ArrayList<Equipment> equipment;
     private ArrayList<Space> spaces;
-    private HashMap<Integer, Space> spaceRentals;
-    private HashMap<Integer, Equipment> equipmentRentals;
+    private HashMap<Integer, SpaceRental> spaceRentals;
+    private HashMap<Integer, EquipmentRental> equipmentRentals;
 
-    public void Controller() {
+    public Controller() {
         equipment = new ArrayList<Equipment>();
+        spaces = new ArrayList<Space>();
+        spaceRentals = new HashMap<>();
+        equipmentRentals = new HashMap<>();
     }
+
+    public int rentSpace(int spaceId, Date time, String musicianName) {
+        return -1;
+    }
+
+    public int rentEquipment(int equipId, Date time, String musicianName) {
+        return -1;
+    }
+
+    public SpaceRental getSpaceRental(int rentalId) {
+        return spaceRentals.get(rentalId);
+    }
+
+    public void viewSpaceSched() {
+
+    }
+
+    public void viewEquipSched() {
+
+    }
+
+    public int addSpace(String description) {
+        return -1;
+    }
+
+    public int addEquipment(String description) {
+        return -1;
+    }
+
+    public void cancelSpace(int rentalId) {
+        
+    }
+
+    public void cancelEquip(int rentalId) {
+
+    }
+    
 }
